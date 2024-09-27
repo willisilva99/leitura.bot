5d2339efb414cb1f955bfc0a79a0aa9c9ee82d78
willisilva99 <126760543+willisilva99@users.noreply.github.com>
2024-09-27 12:37:36 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,86 @@
+import os
+import discord
+from discord.ext import commands, tasks
+import random
+from flask import Flask, jsonify
+import threading
+from flask_cors import CORS
+
+# Pega o token da variável de ambiente
+TOKEN = os.getenv('TOKEN')
+
+# Configurações dos "intents" para o bot
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.presences = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Lista de atividades para o bot
+atividades = [
+    "sobrevivendo ao apocalipse",
+    "jogando 7 Days to Die",
+    "enfrentando zumbis",
+]
+
+# Evento que indica quando o bot está pronto
+@bot.event
+async def on_ready():
+    print(f'Bot conectado como {bot.user}')
+    mudar_atividade.start()
+
+# Tarefa que muda a atividade do bot periodicamente
+@tasks.loop(minutes=5)
+async def mudar_atividade():
+    atividade = random.choice(atividades)
+    await bot.change_presence(activity=discord.Game(name=atividade))
+
+# ---- API Flask ----
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return jsonify({'status': 'Bot está online e rodando!'})
+
+@app.route('/destaques')
+def get_destaques():
+    guild = bot.get_guild(1186390028990025820)  # Substitua pelo ID do seu servidor
+    if guild is None:
+        return jsonify({'error': 'Servidor não encontrado'}), 404
+
+    role = discord.utils.get(guild.roles, name="Destaque")
+    if not role:
+        return jsonify({'error': 'Cargo não encontrado'}), 404
+
+    members = [{'id': member.id, 'username': member.name} for member in role.members]
+    return jsonify(members)
+
+@app.route('/jogadores_online')
+def jogadores_online():
+    guild = bot.get_guild(1186390028990025820)  # Coloque o ID do seu servidor
+    if guild is None:
+        return jsonify({'error': 'Servidor não encontrado'}), 404
+
+    membros_online = [{
+        'id': member.id,
+        'username': member.name,
+        'status': str(member.status).capitalize(),
+    } for member in guild.members if member.status != discord.Status.offline and not member.bot]
+
+    return jsonify(membros_online)
+
+# Função para rodar a API Flask em uma thread separada
+def run_api():
+    port = int(os.environ.get('PORT', 5000))  # Railway usa a variável de ambiente PORT
+    app.run(host='0.0.0.0', port=port)
+
+# Função para rodar o bot e a API ao mesmo tempo
+def run_bot():
+    bot.run(TOKEN)
+
+# Iniciar as threads
+if __name__ == '__main__':
+    threading.Thread(target=run_api).start()
+    run_bot()
